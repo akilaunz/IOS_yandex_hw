1bd8450611d3bfafd09a6f2a0ff4e80a1dd819ac
akilaunz <80848249+akilaunz@users.noreply.github.com>
2022-04-19 17:28:29 +0300

    Add files via upload

---
+++ HW2 (19.04)/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Uikit_akzhan
+//
+//  Created by Акжан Уали on 19.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+
+
+}
+
